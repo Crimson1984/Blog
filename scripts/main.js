d9077b6ddbd911e0c5789d31d35d6f58cac18810
Crimson <crimson1984@foxmail.com>
2025-02-13 20:51:25 +0800

    up grade

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/Dick1.png")
+    {
+        myImage.setAttribute("src", "images/Dick2.png");
+    }
+    else
+    {
+        myImage.setAttribute("src", "images/Dick1.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+function setUserName()
+{
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name"))
+{
+    setUserName();
+}
+else
+{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
